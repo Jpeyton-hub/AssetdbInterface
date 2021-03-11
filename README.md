7cd076059068d0ad522ad994d3d0772f901de738
Jpeyton <jpwalk319@gmail.com>
2021-03-10 20:35:28 -0700

    added seeds

---
+++ README.md
@@ -0,0 +1,13 @@
+># Employee Database Project
+
+---
+
+This project is an exercise in interacting with a database through a node file on the back end. It contains a schema and seed file to produce a small database with three tables and the app itself will prompt you for info and allow you to view the info currently in the database or search for specific groups of data, as well as add to the tables and update existing rows! please find a gif and link to full video below!
+
+---
+
+![gif of usage](./assets\images/gif.gif)
+
+---
+
+[Link to a full video tutorial]()
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,13 @@
-INSERT INTO
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Joe', 'John', 1, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Jim', 'Bo', 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Han', 'Solo', 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Brett', 'Barkley', 3, 2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Hans', 'Gruber', 3, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Bulbus', 'Tumbledrawers', 4, 1);
+INSERT INTO role (title, salary, dept_id) VALUES ('President', 300000, 1);
+INSERT INTO role (title, salary, dept_id) VALUES ('VP', 100000, 2);
+INSERT INTO role (title, salary, dept_id) VALUES ('Lackey', 50000, 2);
+INSERT INTO role (title, salary, dept_id) VALUES ('Wizard', 200000, 3);
+INSERT INTO department (name) VALUES ('Wizard');
+INSERT INTO department (name) VALUES ('Wizard');
+INSERT INTO department (name) VALUES ('Wizard');
