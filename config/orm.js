4d8797b62cce06dda7777d09412756ceb90f2323
Jpeyton <jpwalk319@gmail.com>
2021-03-10 17:54:05 -0700

    added viewemployee by manager func and addemployee func to orm

--- config/orm.js
+++ config/orm.js
@@ -6,25 +6,49 @@ const cTable = require('console.table');
 const orm = {
     viewTable(table){
         let sql = 'SELECT * FROM ??';
-        db.query(sql, [table], (err, response) => {
+        db.query(sql, [table], (err, result) => {
             if(err) {
                 throw err;
             }
-            console.table(response);
+            console.table(result);
         });
     },
 
     viewTableSearch(table, key, value){
         let sql = 'SELECT * FROM ?? WHERE ?? = ?';
-        db.query(sql, [table, key, value], (err, response) => {
+        db.query(sql, [table, key, value], (err, result) => {
             if(err) {
                 throw err;
             }
-            console.table(response);
+            console.table(result);
         });
     },
 
-    viewJoined(){}
+    viewEmployeesByMang(){
+        let sql = 'SELECT employee.first_name, employee.last_name AS employee_name FROM employee RIGHT JOIN employee ON employee.id = employee.manager_id';
+        db.query(sql, (err, result) => {
+            if(err) {
+                throw err;
+            }
+            console.table(result);
+        })
+    },
+
+    addEmployee(firstName, lastName, roleId, managerId){
+        let sql = 'INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)';
+        db.query(sql, [firstName, lastName, roleId, managerId], (err, result) => {
+            if (err) {
+                throw err;
+            }
+            console.log(`${lastName}, ${firstName} was added to Employee database!`);
+        });
+    },
+
+    addDept(){},
+
+    addRole(){},
+
+
 };
 
 module.exports = orm;
